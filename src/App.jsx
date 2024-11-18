c746655843c318b46ebc8f4c5f34fada55d11721
stepchichi <amishra8205@gmail.com>
2024-11-18 21:42:29 +0530

    fixed reloading problem

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [kaam, setKaam] = useState("")
   const [kaams, setKaams] = useState([])
+  const [showFinished, SetshowFinished] = useState(true)
   useEffect(() => {
     let kaamstring = localStorage.getItem("kaams")
     if (kaamstring){
@@ -56,26 +57,32 @@ function App() {
     setKaams(newKaams)
     SaveToLS()
   }
+  const toggleFinished = (e) => {
+    SetshowFinished(!showFinished)
+  }
+  
   
 
   return (
     <>
     <Navbar/>
       <div className="container bg-cyan-100 my-5 mx-auto max-w-screen-lg rounded-xl p-4 min-h-[80vh]">
+        <h1 className='font-bold text-xl text-cyan-900'>Kaamplan - Plan your kaam</h1>
           <div className="addKaam , my-4">
-            <h2 className='text-lg font-bold' >Add Kaam</h2>
+            <h2 className='text-lg font-bold text-cyan-900' >Add Kaam</h2>
           <input onChange= {handleChange} value={kaam} className='w-1/2' type="text" />
-          <button onClick={handleAdd} className='mx-6 bg-cyan-500 hover:bg-cyan-800 text-white p-2 py-1 text-sm rounded-md font-bold'>Save</button>
+          <button onClick={handleAdd} disabled={kaam.length<3} className='mx-6 bg-cyan-500 hover:bg-cyan-800 text-white p-2 py-1 text-sm rounded-md font-bold'>Save</button>
           </div>
-        <h2 className='text-lg font-bold'>Your Kaams</h2> 
+        <input type="checkbox" onChange={toggleFinished} checked={showFinished} /> show finished
+        <h2 className='text-lg font-bold text-cyan-900'>Your Kaams</h2> 
         <div className="kaams">
-        {kaams.length === 0 && <div className=''>No kaam to do</div>}
+        {kaams.length === 0 && <div className='text-cyan-900'>No kaam to do</div>}
         {kaams.map(item =>{ 
 
-          return <div key={item.id} className="kaam flex justify-between my-3">
+          return <div key={item.id} className="kaam flex justify-between my-3 max-w-md">
             <div className='flex gap-5'>
-            <input type="checkbox" onChange={handleCheckbox} value={item.isCompleted} name={item.id} id="" />
-            <div className={item.isCompleted?"line-through":""}>{item.kaam}</div>
+            <input type="checkbox" onChange={handleCheckbox} checked={item.isCompleted} name={item.id} id="" />
+            <div className={`${item.isCompleted ? "line-through" : ""} max-w-md break-words`}>{item.kaam}</div>
             </div>
             <div className="buttons flex h-full">
               <button onClick={(e)=>handleEdit(e,item.id)} className='mx-1 bg-cyan-500 hover:bg-cyan-800 text-white p-2 py-1 text-sm rounded-md font-bold'>Edit</button>
